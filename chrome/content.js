f1a99671f07fdf5c76fa0aef94608050020c339c
Kaspar Minosiants <kaspar.ru@gmail.com>
2012-03-12 11:43:22 +1100

    search radius added

--- chrome/content.js
+++ chrome/content.js
@@ -5,19 +5,21 @@
 		var metaContent=function(prop){
 			return $('meta[property="'+prop+'"]').attr('content');
 		};	
-		 	
-		var geocode=function(){
-			return metaContent("playfoursquare:location:latitude")+","+metaContent("playfoursquare:location:longitude")+",0.250km"; 
-		};
+		 
 		var $container=$("#shareSocial").parent().append("<div style='margin:2px;'></div>");
-		var $query=$("<input class='formStyle notranslate' style='display:inline'></input>").val(metaContent("og:title")).appendTo($container);
-		
-		
+		var $query=$("<input class='formStyle notranslate t4sq-query'></input>").val(metaContent("og:title")).appendTo($container);
+		var $submit=$("<button class='t4sq-submit greenButton' style='display:inline'>Find local tweets</button>").appendTo($container);
+		var $radius=$("<input class='formStyle notranslate t4sq-radius' value='0.250'></input>").appendTo($container);
+		$("<span>km.</span><p class='t4sq-helpText'>Radius of the search</p>").appendTo($container);
+			
+		var geocode=function(){
+			return metaContent("playfoursquare:location:latitude")+","+metaContent("playfoursquare:location:longitude")+","+$radius.val()+"km"; 
+		};
 		
 		var $t=$("<div id='tweets' class='tips'></div>");
 		$('body').append($t);
 		
-		$("<button class='greenButton' style='display:inline'>Find local tweets</button>").click(function(event){
+		$submit.click(function(event){
 			$t.empty().jTweetsAnywhere({
 			    searchParams:['q='+$query.val(), 'geocode='+geocode(), 'result_type=recent'],
 			    count: 20 ,
@@ -37,7 +39,7 @@
 			        }
 			    }			    
 			});
-		}).appendTo($container);
+		});
 		
 	});//document 
 	
